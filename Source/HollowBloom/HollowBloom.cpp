ea0fd6a360997a7a7e6afa350ac0527b1d37e029
wesley0814 <nbowl@naver.com>
2025-10-28 16:56:13 +0900

    Initial commit

---
+++ Source/HollowBloom/HollowBloom.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HollowBloom.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HollowBloom, "HollowBloom" );
